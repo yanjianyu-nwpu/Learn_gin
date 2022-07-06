c9c771fe1db84390052b7140497d127ca3da139c
yanjianyu <545461426@qq.com>
2022-07-06 08:16:20 +0800

    fix: fix

--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
 
 func main() {
-	router := gin.Default()
-
-	// 匹配的url格式:  /welcome?firstname=Jane&lastname=Doe
-	router.GET("/welcome", func(c *gin.Context) {
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // 是 c.Request.URL.Query().Get("lastname") 的简写
-
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
-	})
-	router.Run(":8080")
+	l := rate.NewLimiter(100.0, 100)
+	fmt.Println(l)
 }
